7dc5c7decac2e8cdbf15c6b8860da1b6c1c49a60
luis fernando fontoura spaniol <128423@upf.br>
2018-04-04 20:52:00 -0300

    Add files via upload

---
+++ mandelbrot/rodatudo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+g++ -Wall -std=c++11 -fopenmp $1
+
+echo "$2 $3 $4 $5" > entrada.in
+> tempo.out
+
+for (( i = 1; i <= $6; i++ )); do
+	echo "Executando ${i}ª vez"
+	echo "Execucao $i:" >> tempo.out
+	(time ./a.out < entrada.in > saida.out) 2>> tempo.out
+	echo "" >> tempo.out
+done
+
+while read line;do
+	if [[ $line =~  (real	[0-999][m][])]]; then
+		echo "$line "
+	fi
+done < tempo.out
