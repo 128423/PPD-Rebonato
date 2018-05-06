2499bda56411678d23e5d528cec021703e2968ac
luis fernando fontoura spaniol <128423@upf.br>
2018-05-06 05:42:56 -0300

    Add files via upload

--- mandelmpi.cpp
+++ mandelmpi.cpp
@@ -1,31 +1,14 @@
-#include <complex>
 #include <iostream>
 #include <cstdlib>
 #include <mpi.h>
-
-/* void mandel(int ind, char mat[], int parte, int max_n, int max_row, int max_column)
-{
-	int n;
-	for (int i = 0; i < (parte+ind; ++i)
-	{
-		complex<float> z;
-		n = 0;
-		while (abs(z) < 2 && ++n < max_n)
-			z = pow(z, 2) + decltype(z)(
-								(float)(i % max_column) * 2 / max_column - 1.5,
-								(float)(i / max_column) * 2 / max_row - 1);
-
-		mat[i] = (n == max_n ? '#' : '.');
-	}
+#include <complex>
 
 
-}
- */
 int main(int argc, char * argv[] ){
 
 	int max_row, max_column, max_n,numprocs,ind;
 	int n, parte ;
-	char *mat; 
+	char *vetall; 
 	int max_vet;
 	int procs, meurank;
     int tag=10;
@@ -43,19 +26,21 @@ int main(int argc, char * argv[] ){
 
 	max_vet = max_row *	max_column ;
 	
-	parte =(int) max_vet/(procs);
+	parte = max_vet/(procs);
 
 	ind = parte * meurank ;
+	
 	if (meurank == 0)
 	{
 		vet = (char*)malloc(sizeof(char) * max_vet);
 	}else {
 		vet = (char*)malloc(sizeof(char) * parte);
 	}
+	
 
-	for (int i = 0; i < parte; ++i)
+	for(int i = 0; i <parte; ++i)
 	{
-		complex<float> z;
+		std::complex<float> z;
 		n = 0;
 		while (abs(z) < 2 && ++n < max_n)
 			z = pow(z, 2) + decltype(z)(
@@ -63,17 +48,23 @@ int main(int argc, char * argv[] ){
 								(float)(ind / max_column) * 2 / max_row - 1);
 
 		vet[i] = (n == max_n ? '#' : '.');
-		ind += i ;	
+		ind ++;	
 	}
 
-	MPI_Gather(&vet,parte,MPI_BYTE,vet,parte ,MPI_BYTE,0,MPI_COMM_WORLD);
+	// std::cout << "chegou aqui  " << meurank << '\n';
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	// std::cout << "sinq " << meurank << '\n';
+	MPI_Gather(vet, parte, MPI_BYTE,
+			   vet, parte, MPI_BYTE,
+			   0, MPI_COMM_WORLD);
 
 	if (meurank == 0 ){
 		ind = 0;
 		for (int r = 0; r < max_row; ++r)
 		{
 			for (int c = 0; c < max_column; ++c)
-				std::cout << mat[r * max_column + c];
+				std::cout << vet[r * max_column + c];
 			std::cout << '\n';
 		}
 	
